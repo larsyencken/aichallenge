2fa5cdc48de9503ca8573391253a90b0fd36e8f2
Lars Yencken <lars@yencken.org>
2012-08-23 22:42:29 -0700

    Add some basic Babushka deps.

---
+++ babushka-deps/server.rb
@@ -0,0 +1,95 @@
+dep 'aichallenge-server' do
+    requires \
+        'apache2.managed',
+        'php5.managed',
+        'php5-mysql.managed',
+        'memcached.managed',
+        'php5-memcached.managed',
+        'php5-curl.managed',
+        'zip.managed',
+        'nodejs.managed',
+        'cvs.managed',
+        'openjdk-7-jdk.managed',
+        'ant.managed',
+        'icedtea-plugin.managed',
+        'setuptools.managed',
+        'pip.managed',
+        'dvipng.managed',
+        'texlive-latex-base.managed'
+end
+
+dep 'ant.managed' do
+    installs 'ant'
+end
+
+dep 'apache2.managed' do
+    installs 'apache2'
+    provides []
+end
+
+dep 'cvs.managed' do
+    installs 'cvs'
+end
+
+dep 'dvipng.managed' do
+    installs 'dvipng'
+end
+
+dep 'icedtea-plugin.managed' do
+    installs 'icedtea-plugin'
+    provides []
+end
+
+dep 'memcached.managed' do
+    installs 'memcached'
+    provides []
+end
+
+dep 'nodejs.managed' do
+    installs 'nodejs'
+    provides []
+end
+
+dep 'openjdk-7-jdk.managed' do
+    installs 'openjdk-7-jdk'
+    provides []
+end
+
+dep 'php5.managed' do
+    installs 'php5'
+    provides []
+end
+
+dep 'php5-curl.managed' do
+    installs 'php5-curl'
+    provides []
+end
+
+dep 'php5-memcached.managed' do
+    installs 'php5-memcached'
+    provides []
+end
+
+dep 'php5-mysql.managed' do
+    installs 'php5-mysql'
+    provides []
+end
+
+dep 'pip.managed' do
+    installs 'pip'
+end
+
+dep 'setuptools.managed' do
+    installs 'setuptools'
+    provides ['easy_install']
+end
+
+dep 'texlive-latex-base.managed' do
+    installs 'texlive-latex-base'
+    provides ['latex']
+end
+
+dep 'zip.managed' do
+    installs 'zip'
+end
+
